98ca66658a43259264da6ea71161b94b9be0cc0d
Rafael D.Carmo <rafael.costa@seduc.ce.gov.br>
2022-04-06 08:59:13 -0300

    consertando logo e menus

--- js/init.js
+++ js/init.js
@@ -1,8 +1,20 @@
+
+
+
+
+
 (function($){
   $(function(){
+      
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
   }); // end of document ready
+
+
+  $('.close-sidenav').click(function(){
+    $('.button-collapse').click()
+  })
 })(jQuery); // end of jQuery name space
+
